1e6a625e305ed73e28aa640c3cc7fffb6d890a54
hmjlon <jiyoung110077@gmail.com>
2025-04-24 15:08:12 +0900

    로그인 수정

---
+++ app/community/Login.jsx
@@ -0,0 +1,15 @@
+import { NavBar } from "@/components/nav-bar";
+import { LoginForm } from "@/components/login-form";
+
+export default function LoginPage() {
+  return React.createElement(
+    "main",
+    { className: "min-h-screen bg-traveling-bg" },
+    React.createElement(NavBar, null),
+    React.createElement(
+      "div",
+      { className: "container mx-auto px-4 py-12" },
+      React.createElement(LoginForm, null)
+    )
+  );
+}
