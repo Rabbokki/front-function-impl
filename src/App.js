91b6d09e0f0aacf80a1009b05517a6c67c991832
DESKTOP-QK7FCRU\80415 <sanghoyou@gmail.com>
2025-04-25 11:55:22 +0900

    마이 패이지

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import { Routes, Route } from "react-router-dom"
-import { NavBar } from "./components/Nav-bar"
 import Home from "./pages/Home" // Home.jsx 파일 경로 확인 필수
-import { CommunityContent } from "./components/community/Community-content"
 import TravelPlannerPage from "./pages/travel-planner/TravelMain"
+import MyPage  from "./pages/mypage/MyPage"
 import CommunityPage  from "../src/pages/community/Community"
 import LoginPage  from "../src/pages/login/Login"
 import SignupPage  from "../src/pages/signup/Signup"
@@ -12,12 +11,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/community" element={
-          <div className="container mx-auto px-4 py-8">
-            <h1 className="mb-6 text-3xl font-bold text-[#1e3a8a]">커뮤니티</h1>
-            <CommunityContent />
-          </div>
-        } />
+        <Route path="/mypage" element={<MyPage />} />
         <Route path="/community" element={<CommunityPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
