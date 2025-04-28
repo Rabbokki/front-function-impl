92235280ba62599e0cfe149848453d3480f5aeb9
hmjlon <jiyoung110077@gmail.com>
2025-04-28 11:03:40 +0900

    community

--- src/pages/community/Community.jsx
+++ src/pages/community/Community.jsx
@@ -1,5 +1,5 @@
 import { NavBar } from '../../components/Nav-bar';
-import { CommunityContent } from "../../components/community/Community-content"
+import { CommunityContent } from "../../modules/Community-content";
 
 export default function CommunityPage() {
   return (
