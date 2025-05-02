7bd07c01b851c5d191cbf6e77ebb126c33194f5a
[peryteil] <[peryteil@gmail.com]>
2025-05-02 16:29:38 +0900

    @경로 수정 2

--- src/pages/travel-planner/destination/Step4.jsx
+++ src/pages/travel-planner/destination/Step4.jsx
@@ -1,6 +1,6 @@
 import { NavBar } from "../../../components/Nav-bar";
 import { TransportationSelection } from "../../../components/travel-planner/Transportation-selection";
-import { StepIndicator } from "../../../components/travel-planner/Step-indicator
+import  StepIndicator  from "../../../components/travel-planner/Step-indicator";
 import { notFound } from "next/navigation";
 
 // 지원하는 도시 목록에 새로운 도시들 추가
