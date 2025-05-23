67a360e7c281f1fc7442ebb67f9e95a3d15a5805
Rabbokki <j55267035@gmail.com>
2025-05-23 14:17:27 +0900

    ㅋㅋㅋㅋ

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: 'http://localhost:8080',
   withCredentials: true, // 필요 시 쿠키 포함
 });
 
